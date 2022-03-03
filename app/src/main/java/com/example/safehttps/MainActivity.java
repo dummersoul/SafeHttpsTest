df7e333163df1089eb7cca717b5a4ad87c8635bb
dummersoul <dummersoul@github.com>
2022-03-03 11:31:33 +0800

    换了ttt.com域名又测试了一遍SSLPinning.

--- app/src/main/java/com/example/safehttps/MainActivity.java
+++ app/src/main/java/com/example/safehttps/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 public void run(){
 
                     try {
+                        //利用代码校验证书的公钥hash
 //                        String hostname = "www.gohosts.com";
 //                        CertificatePinner certificatePinner = new CertificatePinner.Builder()
 //                                .add(hostname, "sha256/7VMdvZE3PGbxb0Pgf1PlCp+MI8KZ2ZC5psM8TIylNDA=")
@@ -80,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                                    }
 //                                }).build();
 
+                        //正常okhttp https请求
 //                        OkHttpClient client = new OkHttpClient().newBuilder().hostnameVerifier(new HostnameVerifier() {
 //                            @Override
 //                            public boolean verify(String hostname, SSLSession session) {
@@ -89,8 +91,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                        }).build();
 
 
+                        //利用代码校验证书的公钥证书文件
                         // 获取证书输入流
-                        InputStream openRawResource = getApplicationContext().getResources().openRawResource(R.raw.gohosts); //R.raw.bing是bing.com的正确证书，R.raw.bing2_so是hostname=bing.com的so.com的证书，可视为用作测试的虚假bing.com证书
+                        InputStream openRawResource = getApplicationContext().getResources().openRawResource(R.raw.ttt); //R.raw.bing是bing.com的正确证书，R.raw.bing2_so是hostname=bing.com的so.com的证书，可视为用作测试的虚假bing.com证书
                         Certificate ca = CertificateFactory.getInstance("X.509").generateCertificate(openRawResource);
                         // 创建 Keystore 包含我们的证书
                         KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
@@ -103,12 +106,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         sslContext = SSLContext.getInstance("TLS");  //定义：public static SSLContext sslContext = null;
                         sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
 
-
-
-                        Request request = new Request.Builder()
-                                .url("https://www.gohosts.com/get_data.xml")
-                                .build();
-
                         OkHttpClient client = new OkHttpClient.Builder()
                                 .sslSocketFactory(sslContext.getSocketFactory(),
                                         (X509TrustManager) trustManagerFactory.getTrustManagers()[0] )
@@ -119,6 +116,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                         return true;
                                     }
                                 }).build();
+
+
+
+                        Request request = new Request.Builder()
+                                .url("https://ttt.com/")
+                                .build();
+
 
                         Response response = client.newCall(request).execute();
                         String responseData = response.body().string();
@@ -144,81 +148,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-
-    /**
-     * 对外提供的获取支持自签名的okhttp客户端
-     *
-     * @param certificate 自签名证书的输入流
-     * @return 支持自签名的客户端
-     */
-    public OkHttpClient getTrusClient(InputStream certificate) {
-        X509TrustManager trustManager;
-        SSLSocketFactory sslSocketFactory;
-        try {
-            trustManager = trustManagerForCertificates(certificate);
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            //使用构建出的trustManger初始化SSLContext对象
-            sslContext.init(null, new TrustManager[]{trustManager}, null);
-            //获得sslSocketFactory对象
-            sslSocketFactory = sslContext.getSocketFactory();
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
-        return new OkHttpClient.Builder()
-                .sslSocketFactory(sslSocketFactory, trustManager)
-                .build();
-    }
-
-    /**
-     * 获去信任自签证书的trustManager
-     *
-     * @param in 自签证书输入流
-     * @return 信任自签证书的trustManager
-     * @throws GeneralSecurityException
-     */
-    private X509TrustManager trustManagerForCertificates(InputStream in)
-            throws GeneralSecurityException {
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-        //通过证书工厂得到自签证书对象集合
-        Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(in);
-        if (certificates.isEmpty()) {
-            throw new IllegalArgumentException("expected non-empty set of trusted certificates");
-        }
-        //为证书设置一个keyStore
-        char[] password = "password".toCharArray(); // Any password will work.
-        KeyStore keyStore = newEmptyKeyStore(password);
-        int index = 0;
-        //将证书放入keystore中
-        for (Certificate certificate : certificates) {
-            String certificateAlias = Integer.toString(index++);
-            keyStore.setCertificateEntry(certificateAlias, certificate);
-        }
-        // Use it to build an X509 trust manager.
-        //使用包含自签证书信息的keyStore去构建一个X509TrustManager
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
-                KeyManagerFactory.getDefaultAlgorithm());
-        keyManagerFactory.init(keyStore, password);
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-                TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-            throw new IllegalStateException("Unexpected default trust managers:"
-                    + Arrays.toString(trustManagers));
-        }
-        return (X509TrustManager) trustManagers[0];
-    }
-
-    private KeyStore newEmptyKeyStore(char[] password) throws GeneralSecurityException {
-        try {
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            InputStream in = null; // By convention, 'null' creates an empty key store.
-            keyStore.load(null, password);
-            return keyStore;
-        } catch (IOException e) {
-            throw new AssertionError(e);
-        }
-    }
 
 
 }
